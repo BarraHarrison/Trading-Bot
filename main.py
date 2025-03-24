c0bf41cec249b6985c371955b91a8d4da5a2f1f5
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-24 15:34:08 +0900

    yfinance to connect to the Yahoo Finance data

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 # Trading-Bot to implement Algorithmic Trading Strategy
+import datetime as dt 
+import yfinance as yf 
+import pandas as pd
+from strategy import calculate_emas, generate_signals, backtest_strategy
