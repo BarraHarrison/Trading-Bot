3cbfc6ba38060c37e3145fb1eee2503159b53dc3
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-24 14:38:13 +0900

    Functions to calculate EMAs, generate buy & sell signals and to implement backtesting

--- strategy.py
+++ strategy.py
@@ -1,2 +1,11 @@
 # Algorithmic Trading Strategy
 import pandas as pd
+
+def calculate_emas(data, short=30, long=100):
+    pass
+
+def generate_signals(data, short=30, long=100):
+    pass
+
+def backtest_strategy(data):
+    pass
